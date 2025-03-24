cf12c725d08dd3b615a970c674486ed41722b8b4
Kanstrupm <magnuskanstrup@gmail.com>
2025-03-25 00:12:15 +0100

    Implementing rotate functionality

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    dt = 0
+
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_WIDTH / 2)   
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         dt = clock.tick(60) / 1000
-        screen.fill(000000)
+
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
     
