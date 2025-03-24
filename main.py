b0f9568511a740b52f4c26cf9ba4b810e61d7f25
Kanstrupm <magnuskanstrup@gmail.com>
2025-03-25 00:00:15 +0100

    Added player and drawing of player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import * 
 
 def main():
@@ -9,7 +10,7 @@ def main():
     dt = 0
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_WIDTH / 2)   
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,10 +18,9 @@ def main():
         
         dt = clock.tick(60) / 1000
         screen.fill(000000)
+        player.draw(screen)
         pygame.display.flip()
     
 
 if __name__ == "__main__":
     main()
-
-    
